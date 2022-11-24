22908bf2c96e68393166ac7e500c68ee67089882
Diego Belinski <belinskicosta1@gmail.com>
2022-11-24 00:42:31 -0300

    Resolucao quinto exercicio intro

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicios-intro.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicios-intro.js
@@ -118,3 +118,30 @@ if (currentHour >= 22) {
 console.log(message)
 
 
+
+
+
+/* 
+Exercicio 05
+
+Para fixar
+Crie uma variável chamada “weekDay” que receba a string “quarta-feira”.
+
+Utilizando if/else, implemente condicionais para que:
+
+Se nossa variável “weekDay” for “segunda-feira”, “terça-feira”, “quarta-feira”, 
+“quinta-feira” ou “sexta-feira”, imprima “Oba, mais um dia de aprendizado na Trybe >:D”.
+
+Se for algum dia de fim de semana, imprima “FINALMENTE, descanso merecido UwU”.
+
+Experimente trocar o valor da string ou até mesmo montar seu próprio algoritmo. 
+Esse assunto é muito importante para seu aprendizado :).
+ */
+
+let weekDay = "quarta-feira";
+
+if(weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+} else {
+    console.log("FINALMENTE, descanso merecido UwU");
+}

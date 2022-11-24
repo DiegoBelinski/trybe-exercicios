1926d1b264e6a7c3cd0e260dfe9a7f70a83491ab
Diego Belinski <belinskicosta1@gmail.com>
2022-11-24 01:12:16 -0300

    Resolucao sexto exercicio intro

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicios-intro.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicios-intro.js
@@ -145,3 +145,37 @@ if(weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "qua
 } else {
     console.log("FINALMENTE, descanso merecido UwU");
 }
+
+
+
+
+
+
+/* Exercicio 06
+
+Crie uma variável para armazenar o status da pessoa candidata no processo seletivo, 
+que pode ser: 'aprovada', 'lista' ou 'reprovada';
+Crie uma estrutura condicional com o switch/case que irá imprimir as seguintes mensagens:
+Caso 'aprovada', imprima “Parabéns, você foi aprovada(o)!”.
+Caso 'lista', imprima “Você está na nossa lista de espera”.
+Caso 'reprovada', imprima “Você foi reprovada(o)”.
+Caso default, imprima a mensagem de “Informação incorreta”. */
+
+statusCandidato = "Aprovada"
+
+switch(statusCandidato) {
+    case "Aprovada":
+        console.log('Parabéns, você foi aprovada(o)!')
+        break;
+       
+    case "lista":
+        console.log('Você está na nossa lista de espera')
+        break;
+
+    case "reprovada":
+        console.log('Você foi reprovada(o)')
+        break;
+
+    default: 
+    console.log('Informação incorreta')
+}

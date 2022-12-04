2cceddc939eee142aaeb1f25083a3208d29a257f
DiegoBelinski <117599266+DiegoBelinski@users.noreply.github.com>
2022-12-04 20:17:24 -0300

    Update exercicio-extra.js

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio-extra.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio-extra.js
@@ -14,3 +14,109 @@ for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
  */
 
 
+
+
+
+/* Faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
+
+let size = 5;
+let symbol = `*`;
+let imputLine = "";
+
+for ( let index = 0; index <= size; index += 1 ){
+ imputLine = imputLine + symbol
+ console.log(imputLine)
+} */
+
+
+
+
+/* 
+Agora, inverta o lado do triângulo.
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+let inputPosition = n - 1;
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex < n; columnIndex += 1) {
+    if (columnIndex < inputPosition) {
+      inputLine = inputLine + ' ';
+    } else {
+      inputLine = inputLine + symbol;
+    }
+  }
+  console.log(inputLine);
+  inputLine = '';
+  inputPosition -= 1;
+};
+*/
+
+
+
+
+/*
+Depois, faça uma pirâmide com n asteriscos de base
+
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+
+let midOfMatrix = (n + 1) / 2;
+let controlLeft = midOfMatrix;
+let controlRight = midOfMatrix;
+
+for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+    if (columnIndex > controlLeft && columnIndex < controlRight) {
+      inputLine = inputLine + symbol;
+    } else {
+      inputLine = inputLine + ' ';
+    }
+  }
+  console.log(inputLine);
+  inputLine = '';
+  controlRight += 1;
+  controlLeft -= 1
+};
+*/
+
+
+
+/*
+Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar
+let n = 7;
+let middle = (n + 1) / 2;
+let controlLeft = middle;
+let controlRight = middle;
+let symbol = '*';
+for (let line = 1; line <= middle; line += 1) {
+  let outputLine = '';
+  for (let col = 1; col <= n; col += 1) {
+    if (col == controlLeft || col == controlRight || line == middle) {
+      outputLine += symbol;
+    } else {
+      outputLine += ' ';
+    }
+  }
+  controlLeft -= 1;
+  controlRight += 1;
+  console.log(outputLine);
+}
+*/
+
+
+
+
+/*
+Faça um programa que diz se um número definido numa variável é primo ou não.
+let divisors = 1;
+let numberToCheck = 31;
+
+for (let number = 2; number <= numberToCheck; number += 1) {
+  if (numberToCheck % number === 0) divisors += 1;
+}
+
+if (divisors === 2) console.log(numberToCheck + ' é primo');
+else console.log(numberToCheck + ' não é primo');
+*/

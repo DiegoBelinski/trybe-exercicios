6b18906e14e8634534b735eee728f18d95ef008f
Diego Belinski <belinskicosta1@gmail.com>
2022-12-06 02:34:31 -0300

    Exercicios de Funcoes

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicio-funcoes.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicio-funcoes.js
@@ -1,1 +1,19 @@
+/* Crie uma função que receba uma string e retorne true se for um palíndromo,
+ou false, se não for. */
 
+
+function verificaPalindrome (string){
+
+    let spl = string.split ('') 
+    let rev = spl.reverse('')
+    let joi = rev.join('')
+        
+    if ( joi === string) { 
+        return true
+    } else {
+        return false
+    }
+
+}
+    
+    console.log(verificaPalindrome('arara'))
